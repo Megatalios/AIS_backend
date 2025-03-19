4a3f723d3f69539777ce8a3e5e806a5b56ce7f7c
Rinat7002 <rinat_idrisov_2016@mail.ru>
2025-03-19 20:59:10 +0500

    register models for django admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User  # Импортируй нужные модели
+
+# admin.site.register(User)  # Регистрируем модель в админке
+
+
